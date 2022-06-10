eef3973efed2e3fa320273d7defa2ba560b8728a
kirill-herman <kirill.herman.public@gmail.com>
2022-06-10 09:44:02 +0300

    feat: fill routes array

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { VacationComponent } from './pages/vacation/vacation.component';
+import { StatisticComponent } from './pages/statistic/statistic.component';
+import { ReportsComponent } from './pages/reports/reports.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { FinanceComponent } from './pages/finance/finance.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'vacation', component: VacationComponent },
+  { path: 'statistic', component: StatisticComponent },
+  { path: 'reports', component: ReportsComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'finance', component: FinanceComponent },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'dashboard' } // route for 404 page in future
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
