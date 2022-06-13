0d59aace09cfdc7364ac12b2556134b9df116707
kirill-herman <kirill.herman.public@gmail.com>
2022-06-13 10:34:41 +0300

    feat: make lazy loading

--- src/app/pages/vacation/vacation.module.ts
+++ src/app/pages/vacation/vacation.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VacationComponent } from './vacation.component';
+import { VacationRoutingModule } from './vacation-routing.module';
 
 
 
@@ -9,7 +10,8 @@ import { VacationComponent } from './vacation.component';
     VacationComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    VacationRoutingModule,
   ]
 })
 export class VacationModule { }
