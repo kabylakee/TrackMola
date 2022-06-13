b9fb63ce16176d135d433e859cae3ef458e37ebc
kirill-herman <kirill.herman.public@gmail.com>
2022-06-13 10:47:47 +0300

    refactor: resolve conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { AppComponent } from './app.component';
 import { HeaderModule } from './shared/components/header/header.module';
 @NgModule({
   declarations: [
@@ -12,6 +12,7 @@ import { HeaderModule } from './shared/components/header/header.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     HeaderModule,
   ],
   providers: [],
