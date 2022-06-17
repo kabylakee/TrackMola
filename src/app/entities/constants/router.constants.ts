fe41589806e7356e980b9d1ea16fe6a6e1e6e865
kirill-herman <kirill.herman.public@gmail.com>
2022-06-17 15:26:20 +0300

    refactor: change nav-link title to string

--- src/app/entities/constants/router.constants.ts
+++ src/app/entities/constants/router.constants.ts
@@ -3,37 +3,37 @@ import {RouterPaths, NavigationIcons} from '../enums/router.enum';
 export const NAVIGATION_LINKS = [
 	{
 		path: RouterPaths.Dashboard,
-		title: RouterPaths.Dashboard[0].toUpperCase() + RouterPaths.Dashboard.slice(1),
+		title: 'Dashboard',
 		icon: NavigationIcons.Dashboard,
 	},
 	{
 		path: RouterPaths.Projects,
-		title: RouterPaths.Projects[0].toUpperCase() + RouterPaths.Projects.slice(1),
+		title: 'Projects',
 		icon: NavigationIcons.Projects,
 	},
 	{
 		path: RouterPaths.Reports,
-		title: RouterPaths.Reports[0].toUpperCase() + RouterPaths.Reports.slice(1),
+		title: 'Reports',
 		icon: NavigationIcons.Reports,
 	},
 	{
 		path: RouterPaths.Vacation,
-		title: RouterPaths.Vacation[0].toUpperCase() + RouterPaths.Vacation.slice(1),
+		title: 'Vacation',
 		icon: NavigationIcons.Vacation,
 	},
 	{
 		path: RouterPaths.Statistic,
-		title: RouterPaths.Statistic[0].toUpperCase() + RouterPaths.Statistic.slice(1),
+		title: 'Statistic',
 		icon: NavigationIcons.Statistic,
 	},
 	{
 		path: RouterPaths.Finance,
-		title: RouterPaths.Finance[0].toUpperCase() + RouterPaths.Finance.slice(1),
+		title: 'Finance',
 		icon: NavigationIcons.Finance,
 	},
 	{
 		path: RouterPaths.Settings,
-		title: RouterPaths.Settings[0].toUpperCase() + RouterPaths.Settings.slice(1),
+		title: 'Settings',
 		icon: NavigationIcons.Settings,
 	},
 ];
