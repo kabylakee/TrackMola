3444ee1c6c86afb3de0139fd064110572ec0a357
kirill-herman <kirill.herman.public@gmail.com>
2022-06-17 13:15:07 +0300

    feat: add enum for nav-icons

---
+++ src/app/entities/enums/nav-icon.enum.ts
@@ -0,0 +1,9 @@
+export enum NavigationIcons {
+	Dashboard = 'dashboard',
+	Projects = 'view_timeline',
+	Reports = 'schedule',
+	Vacation = 'perm_contact_calendar',
+	Statistic = 'stacked_line_chart',
+	Finance = 'attach_money',
+	Settings = 'admin_panel_settings',
+}
--- src/app/shared/components/left-panel/left-panel.component.ts
+++ src/app/shared/components/left-panel/left-panel.component.ts
@@ -2,6 +2,7 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {KeyValue} from '@angular/common';
 
 import {RouterPaths} from 'src/app/entities/enums/router.enum';
+import {NavigationIcons} from 'src/app/entities/enums/nav-icon.enum';
 
 @Component({
 	selector: 'app-left-panel',
@@ -10,18 +11,37 @@ import {RouterPaths} from 'src/app/entities/enums/router.enum';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LeftPanelComponent {
-	public readonly paths = RouterPaths;
-
 	public isPanelSmall = true;
 
-	public readonly icons = [
-		'dashboard',
-		'view_timeline',
-		'schedule',
-		'perm_contact_calendar',
-		'stacked_line_chart',
-		'attach_money',
-		'admin_panel_settings',
+	public readonly navigationLinks = [
+		{
+			path: RouterPaths.Dashboard,
+			icon: NavigationIcons.Dashboard,
+		},
+		{
+			path: RouterPaths.Projects,
+			icon: NavigationIcons.Projects,
+		},
+		{
+			path: RouterPaths.Reports,
+			icon: NavigationIcons.Reports,
+		},
+		{
+			path: RouterPaths.Vacation,
+			icon: NavigationIcons.Vacation,
+		},
+		{
+			path: RouterPaths.Statistic,
+			icon: NavigationIcons.Statistic,
+		},
+		{
+			path: RouterPaths.Finance,
+			icon: NavigationIcons.Finance,
+		},
+		{
+			path: RouterPaths.Settings,
+			icon: NavigationIcons.Settings,
+		},
 	];
 
 	public originalOrderComparator = (
