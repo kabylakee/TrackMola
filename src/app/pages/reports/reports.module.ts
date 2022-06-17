fe137157245ecfc1f6b1b0a57b7e1401a9261432
Musa98 <gsunnymus@gmail.com>
2022-06-17 10:24:22 +0300

    feat: add toggle button

--- src/app/pages/reports/reports.module.ts
+++ src/app/pages/reports/reports.module.ts
@@ -2,11 +2,10 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ReportsComponent} from './reports.component';
 import {ReportsRoutingModule} from './reports-routing.module';
-import {ToggleButtonComponent} from './toggle-button/toggle-button.component';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {ToggleButtonModule} from 'src/app/shared/components/toggle-button/toggle-button.module';
 
 @NgModule({
-	declarations: [ReportsComponent, ToggleButtonComponent],
-	imports: [CommonModule, ReportsRoutingModule, MatButtonToggleModule],
+	declarations: [ReportsComponent],
+	imports: [CommonModule, ReportsRoutingModule, ToggleButtonModule],
 })
 export class ReportsModule {}
