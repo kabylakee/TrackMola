4051468741d2ccbff9f9ce21f28f0875d7158fea
kirill-herman <kirill.herman.public@gmail.com>
2022-06-17 10:10:36 +0300

    fix: use mat-icon

--- src/app/shared/components/left-panel/left-panel.component.ts
+++ src/app/shared/components/left-panel/left-panel.component.ts
@@ -1,31 +1,37 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {KeyValue} from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { KeyValue } from '@angular/common';
 
-import {RouterPaths} from 'src/app/entities/enums/router.enum';
+import { RouterPaths } from 'src/app/entities/enums/router.enum';
 
 @Component({
-	selector: 'app-left-panel',
-	templateUrl: './left-panel.component.html',
-	styleUrls: [
-		'./styles/left-panel.component.scss',
-		'./styles/left-panel-icons.component.scss',
-		'./styles/left-panel-small.component.scss',
-	],
-	changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: 'app-left-panel',
+  templateUrl: './left-panel.component.html',
+  styleUrls: ['./styles/left-panel.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LeftPanelComponent {
-	public paths = RouterPaths;
+  public paths = RouterPaths;
 
-	public isPanelSmall = true;
+  public isPanelSmall = true;
 
-	public originalOrderComparator = (
-		a: KeyValue<string, string>,
-		b: KeyValue<string, string>,
-	): number => {
-		return 0;
-	};
+  public icons = [
+    'dashboard',
+    'view_timeline',
+    'schedule',
+    'perm_contact_calendar',
+    'stacked_line_chart',
+    'attach_money',
+    'admin_panel_settings',
+  ];
 
-	public switchMenuView(): void {
-		this.isPanelSmall = !this.isPanelSmall;
-	}
+  public originalOrderComparator = (
+    a: KeyValue<string, string>,
+    b: KeyValue<string, string>,
+  ): number => {
+    return 0;
+  };
+
+  public switchMenuView(): void {
+    this.isPanelSmall = !this.isPanelSmall;
+  }
 }
