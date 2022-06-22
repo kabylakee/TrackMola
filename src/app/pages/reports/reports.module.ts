7eb05a791e87621807225f848950b05af681be40
kirill-herman <kirill.herman.public@gmail.com>
2022-06-22 17:29:39 +0300

    refactor: delete all example, move styles to scss/material/mat-button.scss

--- src/app/pages/reports/reports.module.ts
+++ src/app/pages/reports/reports.module.ts
@@ -3,11 +3,9 @@ import {CommonModule} from '@angular/common';
 import {ReportsComponent} from './reports.component';
 import {ReportsRoutingModule} from './reports-routing.module';
 import {TableFilterModule} from 'src/app/shared/components/table-filter/table-filter.module';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
 	declarations: [ReportsComponent],
-	imports: [CommonModule, ReportsRoutingModule, TableFilterModule, MatButtonModule, MatIconModule],
+	imports: [CommonModule, ReportsRoutingModule, TableFilterModule],
 })
 export class ReportsModule {}
