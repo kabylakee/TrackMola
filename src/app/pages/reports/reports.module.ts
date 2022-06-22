e9958f4ad03a307afaf78b8b47780f71e1f6490a
kirill-herman <kirill.herman.public@gmail.com>
2022-06-22 16:57:41 +0300

    create global styles for mat-button, add exemple of local changes to reports page

--- src/app/pages/reports/reports.module.ts
+++ src/app/pages/reports/reports.module.ts
@@ -1,11 +1,13 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {ReportsComponent} from './reports.component';
-import {ReportsRoutingModule} from './reports-routing.module';
-import {TableFilterModule} from 'src/app/shared/components/table-filter/table-filter.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReportsComponent } from './reports.component';
+import { ReportsRoutingModule } from './reports-routing.module';
+import { TableFilterModule } from 'src/app/shared/components/table-filter/table-filter.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
 	declarations: [ReportsComponent],
-	imports: [CommonModule, ReportsRoutingModule, TableFilterModule],
+	imports: [CommonModule, ReportsRoutingModule, TableFilterModule, MatButtonModule, MatIconModule],
 })
-export class ReportsModule {}
+export class ReportsModule { }
