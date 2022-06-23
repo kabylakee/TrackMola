8188fca0178e6ecb3108e54286a70c4d5b95f141
Musa98 <gsunnymus@gmail.com>
2022-06-23 11:00:42 +0300

    feat: add component structure and fix stylelint error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import {MatButtonModule} from '@angular/material/button';
 
 import {AppComponent} from './app.component';
 import {LeftPanelModule} from './shared/components/left-panel/left-panel.module';
+import {DatePickerComponent} from './app/shared/components/date-picker/date-picker.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, DatePickerComponent],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
