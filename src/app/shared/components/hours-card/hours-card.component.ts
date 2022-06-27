993ed4201882a692c32cac0bfc6989c245393c5c
kirill-herman <kirill.herman.public@gmail.com>
2022-06-27 08:34:49 +0300

    feat: rendering with ngFor

--- src/app/shared/components/hours-card/hours-card.component.ts
+++ src/app/shared/components/hours-card/hours-card.component.ts
@@ -1,11 +1,40 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-hours-card',
   templateUrl: './hours-card.component.html',
   styleUrls: ['./hours-card.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HoursCardComponent {
+export class HoursCardComponent implements OnChanges {
+  @Input()
+  totalHours = 0;
 
+  public hourColor = 'red';
+
+  public hourCards = [{ title: 'Total Hours', hours: 0 }];
+
+  private readonly normalHoursCount = 8;
+
+  ngOnChanges() {
+    switch (true) {
+      case this.totalHours < this.normalHoursCount:
+        this.hourColor = 'red';
+        this.hourCards = [{ title: 'Total Hours', hours: this.totalHours }];
+        break;
+      case this.totalHours === this.normalHoursCount:
+        this.hourColor = 'green';
+        this.hourCards = [{ title: 'Total Hours', hours: this.totalHours }];
+        break;
+      case this.totalHours > this.normalHoursCount:
+        this.hourColor = 'orange';
+        this.hourCards = [
+          { title: 'Total Hours', hours: this.totalHours },
+          { title: 'Overtime Hours', hours: this.totalHours - this.normalHoursCount },
+        ];
+        break;
+      default:
+        break;
+    }
+  }
 }
