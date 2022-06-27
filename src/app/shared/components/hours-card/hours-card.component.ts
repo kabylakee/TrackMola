8a05e1bccaefa91eb2839357877e76fda0a44a74
kirill-herman <kirill.herman.public@gmail.com>
2022-06-27 10:06:54 +0300

    feat: rendering overtime with ngIf

--- src/app/shared/components/hours-card/hours-card.component.ts
+++ src/app/shared/components/hours-card/hours-card.component.ts
@@ -1,40 +1,32 @@
-import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnChanges} from '@angular/core';
 
 @Component({
-  selector: 'app-hours-card',
-  templateUrl: './hours-card.component.html',
-  styleUrls: ['./hours-card.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+	selector: 'app-hours-card',
+	templateUrl: './hours-card.component.html',
+	styleUrls: ['./hours-card.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HoursCardComponent implements OnChanges {
-  @Input()
-  totalHours = 0;
+	@Input()
+	totalHours = 0;
 
-  public hourColor = 'red';
+	public hourStatus = 'not-enough';
 
-  public hourCards = [{ title: 'Total Hours', hours: 0 }];
+	public readonly normalHoursCount = 8;
 
-  private readonly normalHoursCount = 8;
-
-  ngOnChanges() {
-    switch (true) {
-      case this.totalHours < this.normalHoursCount:
-        this.hourColor = 'red';
-        this.hourCards = [{ title: 'Total Hours', hours: this.totalHours }];
-        break;
-      case this.totalHours === this.normalHoursCount:
-        this.hourColor = 'green';
-        this.hourCards = [{ title: 'Total Hours', hours: this.totalHours }];
-        break;
-      case this.totalHours > this.normalHoursCount:
-        this.hourColor = 'orange';
-        this.hourCards = [
-          { title: 'Total Hours', hours: this.totalHours },
-          { title: 'Overtime Hours', hours: this.totalHours - this.normalHoursCount },
-        ];
-        break;
-      default:
-        break;
-    }
-  }
+	ngOnChanges() {
+		switch (true) {
+			case this.totalHours < this.normalHoursCount:
+				this.hourStatus = 'not-enough';
+				break;
+			case this.totalHours === this.normalHoursCount:
+				this.hourStatus = 'enough';
+				break;
+			case this.totalHours > this.normalHoursCount:
+				this.hourStatus = 'overtime';
+				break;
+			default:
+				break;
+		}
+	}
 }
