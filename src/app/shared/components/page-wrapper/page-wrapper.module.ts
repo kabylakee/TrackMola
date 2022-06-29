653b0cbad95fee0957959d6e782a1aff6cefe6f2
Tatiana Kudryavets <minsk_tania@mail.ru>
2022-06-29 20:41:52 +0300

    Unnecessary imports deleted

--- src/app/shared/components/page-wrapper/page-wrapper.module.ts
+++ src/app/shared/components/page-wrapper/page-wrapper.module.ts
@@ -1,14 +1,11 @@
 import {NgModule} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
 import {AppRoutingModule} from 'src/app/app-routing.module';
 import {HeaderModule} from '../header/header.module';
-import {LeftPanelModule} from '../left-panel/left-panel.module';
 import {PageWrapperComponent} from './page-wrapper.component';
 
 @NgModule({
 	declarations: [PageWrapperComponent],
-	imports: [HeaderModule, AppRoutingModule, MatIconModule, MatButtonModule, LeftPanelModule],
+	imports: [HeaderModule, AppRoutingModule,],
 	exports: [PageWrapperComponent],
 })
 export class PageWrapperModule {}
