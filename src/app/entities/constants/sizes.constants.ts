5b510c0be9d549cc73bbe105a567507933022a87
Tatiana Kudryavets <minsk_tania@mail.ru>
2022-07-01 12:45:35 +0300

    config changed

--- src/app/entities/constants/sizes.constants.ts
+++ src/app/entities/constants/sizes.constants.ts
@@ -4,19 +4,19 @@ import {ISizeConfig} from '../interfaces/size-config.interface';
 
 export const SIZES_CONFIG: AssociativeObject<ISizeConfig> = {
 	[Size.S]: {
-		height: 0.75,
-		width: 1,
-	},
-	[Size.M]: {
-		height: 1.125,
-		width: 1.5,
-	},
-	[Size.L]: {
 		height: 1.5,
 		width: 2,
 	},
-	[Size.Xl]: {
+	[Size.M]: {
 		height: 3,
 		width: 4,
+	},
+	[Size.L]: {
+		height: 4.5,
+		width: 6,
+	},
+	[Size.Xl]: {
+		height: 6.75,
+		width: 9,
 	},
 };
