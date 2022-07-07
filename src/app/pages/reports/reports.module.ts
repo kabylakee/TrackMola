068db92b20cc0fc7c4b165dd7eaaa525db4a42be
Tatiana Kudryavets <minsk_tania@mail.ru>
2022-07-07 14:25:36 +0300

    clean some files

--- src/app/pages/reports/reports.module.ts
+++ src/app/pages/reports/reports.module.ts
@@ -3,7 +3,6 @@ import {CommonModule} from '@angular/common';
 import {ReportsComponent} from './reports.component';
 import {ReportsRoutingModule} from './reports-routing.module';
 import {TableFilterModule} from 'src/app/shared/components/table-filter/table-filter.module';
-import {ReportFilterModule} from 'src/app/shared/components/report-filter/report-filter.module';
 import {MonthTableModule} from 'src/app/shared/components/month-table/month-table.module';
 import {ReportsTableModule} from 'src/app/shared/components/reports-table/reports-table.module';
 
@@ -15,7 +14,6 @@ import {ReportsTableModule} from 'src/app/shared/components/reports-table/report
 		TableFilterModule,
 		ReportsTableModule,
 		MonthTableModule,
-		ReportFilterModule,
 	],
 })
 export class ReportsModule {}
