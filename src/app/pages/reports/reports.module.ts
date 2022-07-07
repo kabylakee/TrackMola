31c87a15fe249f4938ce91fc25ce7974ed4d04bf
kirill-herman <kirill.herman.public@gmail.com>
2022-07-07 13:35:43 +0300

    feat: add global color vars for day type

--- src/app/pages/reports/reports.module.ts
+++ src/app/pages/reports/reports.module.ts
@@ -1,10 +1,11 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {ReportsComponent} from './reports.component';
-import {ReportsRoutingModule} from './reports-routing.module';
-import {TableFilterModule} from 'src/app/shared/components/table-filter/table-filter.module';
-import {MonthTableModule} from 'src/app/shared/components/month-table/month-table.module';
-import {ReportsTableModule} from 'src/app/shared/components/reports-table/reports-table.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReportsComponent } from './reports.component';
+import { ReportsRoutingModule } from './reports-routing.module';
+import { TableFilterModule } from 'src/app/shared/components/table-filter/table-filter.module';
+import { MonthTableModule } from 'src/app/shared/components/month-table/month-table.module';
+import { ReportsTableModule } from 'src/app/shared/components/reports-table/reports-table.module';
+import { CalendarLegendModule } from 'src/app/shared/components/calendar-legend/calendar-legend.module';
 
 @NgModule({
 	declarations: [ReportsComponent],
@@ -14,6 +15,7 @@ import {ReportsTableModule} from 'src/app/shared/components/reports-table/report
 		TableFilterModule,
 		ReportsTableModule,
 		MonthTableModule,
+		CalendarLegendModule,
 	],
 })
-export class ReportsModule {}
+export class ReportsModule { }
