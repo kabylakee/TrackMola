a85cad21fc592de7574fb50477b083095fbdd9cd
kirill-herman <kirill.herman.public@gmail.com>
2022-07-07 11:11:24 +0300

    feat: create calendar component

---
+++ src/app/shared/components/calendar-legend/calendar-legend.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-legend',
+  templateUrl: './calendar-legend.component.html',
+  styleUrls: ['./calendar-legend.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CalendarLegendComponent { }
---
+++ src/app/shared/components/calendar-legend/calendar-legend.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CalendarLegendComponent } from './calendar-legend.component';
+
+@NgModule({
+  declarations: [CalendarLegendComponent],
+  imports: [CommonModule],
+  exports: [CalendarLegendComponent],
+})
+export class CalendarLegendModule { }
