19e37676d600a064f25acf09bbb023e93ecff72f
Tatiana Kudryavets <minsk_tania@mail.ru>
2022-07-09 20:46:55 +0300

    Default value deleted

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -7,5 +7,5 @@ import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
-	@Input() pageTitle: string = '';
+	@Input() pageTitle: string;
 }
