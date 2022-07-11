6b1bcd68108fa80b15c3da464546cfdcbae4c2cc
kirill-herman <kirill.herman.public@gmail.com>
2022-07-11 17:16:50 +0300

    feat: add output

--- src/app/shared/components/reports-button/reports-button.component.ts
+++ src/app/shared/components/reports-button/reports-button.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
 import {ReportsButtonEnum} from 'src/app/entities/enums/reports-button.enum';
 
 @Component({
@@ -11,5 +11,25 @@ export class ReportsButtonComponent {
 	@Input() canSubmit = false;
 	@Input() showNotifier = false;
 
+	@Output() addTask = new EventEmitter<void>();
+	@Output() saveReport = new EventEmitter<void>();
+	@Output() submitReport = new EventEmitter<void>();
+
 	public readonly buttons = ReportsButtonEnum;
+
+	public onClick(button: ReportsButtonEnum): void {
+		switch (button) {
+			case ReportsButtonEnum.AddTask:
+				this.addTask.emit();
+				break;
+			case ReportsButtonEnum.Save:
+				this.saveReport.emit();
+				break;
+			case ReportsButtonEnum.Submit:
+				this.submitReport.emit();
+				break;
+			default:
+				break;
+		}
+	}
 }
