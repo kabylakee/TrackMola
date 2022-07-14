7e10e8651b69844dea88c5498590fdb544fa9f83
Vika <vctrcute@gmail.com>
2022-07-14 15:15:25 +0300

    fixes

--- src/app/shared/services/localStorage.service.ts
+++ src/app/shared/services/localStorage.service.ts
@@ -3,7 +3,7 @@ import {Injectable} from '@angular/core';
 @Injectable({providedIn: 'root'})
 export class LocalStorageService {
 	public getData(key: string): unknown {
-		return JSON.parse(localStorage.getItem(key) || 'null');
+		return JSON.parse(localStorage.getItem(key)!);
 	}
 
 	public setData(key: string, data: unknown): void {
