3c933b92f2a0233f073662bf9b3029dae7bf1390
kirill-herman <kirill.herman.public@gmail.com>
2022-07-15 00:03:35 +0300

    fix: add flex wrapper to calendar

--- src/app/shared/components/month-table/month-table.component.ts
+++ src/app/shared/components/month-table/month-table.component.ts
@@ -1,4 +1,5 @@
 import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {DayTypeEnum} from 'src/app/entities/enums/day-type.enum';
 import {WeekDayEnum} from 'src/app/entities/enums/week-day.enum';
 import {IReportsDayInfo} from 'src/app/entities/interfaces/reports-day-info.interface';
 
@@ -16,6 +17,7 @@ export class MonthTableComponent {
 	@Output() selectDay = new EventEmitter<Date>();
 
 	public readonly weekDay = Object.values(WeekDayEnum);
+	public readonly legendItems = Object.values(DayTypeEnum);
 
 	public onSelectDay(date: Date): void {
 		this.selectDay.emit(date);
