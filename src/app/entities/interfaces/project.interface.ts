1e814b5f6463f92684038d6aa1e321a1f3bc5594
Tatiana Kudryavets <minsk_tania@mail.ru>
2022-07-16 15:44:44 +0300

    comments deleted

--- src/app/entities/interfaces/project.interface.ts
+++ src/app/entities/interfaces/project.interface.ts
@@ -1,9 +1,5 @@
-// TODO: import {IEmployee} from './employee.interface';
-
 export interface IProject {
 	title: string;
 	checked?: boolean;
 	percentage?: number;
-	// TODO: employees: IEmployee[];
-	// TODO: lead: IEmployee;
 }
