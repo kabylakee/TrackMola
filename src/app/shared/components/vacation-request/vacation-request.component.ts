c133755d3565c35755e6ba3cd558fba4ab6b20ee
Tatiana Kudryavets <minsk_tania@mail.ru>
2022-07-16 10:58:18 +0300

    this.notification deleted

--- src/app/shared/components/vacation-request/vacation-request.component.ts
+++ src/app/shared/components/vacation-request/vacation-request.component.ts
@@ -18,8 +18,7 @@ export class VacationRequestComponent {
 	@Output() sendRequest = new EventEmitter();
 
 	public requestForm: FormGroup;
-	public field: string;
-	public notification = false;
+	public message: string;
 
 	constructor(
 		private dialogRef: MatDialogRef<VacationRequestComponent>,
@@ -46,7 +45,9 @@ export class VacationRequestComponent {
 	}
 
 	formValidator(control: AbstractControl): ValidationErrors | null {
-		if (!control.get('dateFrom')?.value || !control.get('dateTo')?.value) return {error: true};
+		if (!control.get('dateFrom')?.value || !control.get('dateTo')?.value) {
+			return {error: true};
+		}
 		if (control.get('dateTo')?.value >= control.get('dateFrom')?.value) {
 			return null;
 		}
@@ -55,22 +56,17 @@ export class VacationRequestComponent {
 
 	public onSend(): void {
 		if (!this.requestForm.controls.dateFrom.valid) {
-			this.field = 'Date from is invalid';
-			this.notification = true;
+			this.message = 'Date from is invalid';
 			return;
 		}
 		if (!this.requestForm.controls.dateTo.valid) {
-			this.field = 'Date to is invalid';
-			this.notification = true;
+			this.message = 'Date to is invalid';
 			return;
 		}
 		if (!this.requestForm.valid) {
-			this.field = 'Date to is invalid';
-			this.notification = true;
+			this.message = 'Date to is invalid';
 			return;
 		}
-
-		this.notification = false;
 		this.sendRequest.emit(this.requestForm);
 		this.dialogRef.close();
 	}
