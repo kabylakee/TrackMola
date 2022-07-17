ff72c536b35aec068ed4b5aecbf0f26870af53f7
Tatiana Kudryavets <minsk_tania@mail.ru>
2022-07-17 19:49:48 +0300

    no-data added

--- src/app/shared/components/reports-table/reports-table.module.ts
+++ src/app/shared/components/reports-table/reports-table.module.ts
@@ -10,6 +10,7 @@ import {MatMenuModule} from '@angular/material/menu';
 import {MatSelectModule} from '@angular/material/select';
 import {MatDialogModule} from '@angular/material/dialog';
 
+import {NoDataModule} from 'src/app/shared/components/no-data/no-data.module';
 import {ReportsTableComponent} from './reports-table.component';
 import {ProjectComponent} from '../../components/project/project.component';
 import {LinkDialogComponent} from '../link-dialog/link-dialog.component';
@@ -30,6 +31,7 @@ import {CdkTableModule} from '@angular/cdk/table';
 		CdkTableModule,
 		MatSelectModule,
 		MatDialogModule,
+		NoDataModule,
 	],
 	exports: [ReportsTableComponent],
 })
