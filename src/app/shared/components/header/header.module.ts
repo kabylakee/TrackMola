14f06d8e324582ea90143817fbf08b31ca9cace2
YauheniZhybryk <yzhybryk@datamola.com>
2022-07-18 00:30:45 +0300

    fixes

--- src/app/shared/components/header/header.module.ts
+++ src/app/shared/components/header/header.module.ts
@@ -1,14 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HeaderComponent} from './header.component';
-import {BellModule} from '../bell/bell.module';
 
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {NotificationModule} from '../notification/notification.module';
 
 @NgModule({
 	declarations: [HeaderComponent],
-	imports: [CommonModule, MatIconModule, MatButtonModule, BellModule],
+	imports: [CommonModule, MatIconModule, MatButtonModule, NotificationModule],
 	exports: [HeaderComponent],
 })
 export class HeaderModule {}
--- src/app/shared/components/notification/notification.component.ts
+++ src/app/shared/components/notification/notification.component.ts
@@ -4,8 +4,6 @@ import {
 	Component,
 	EventEmitter,
 	Input,
-	OnChanges,
-	OnInit,
 	Output,
 } from '@angular/core';
 import {INotification} from 'src/app/entities/interfaces/notification.interface';
@@ -16,59 +14,27 @@ import {TestService} from '../../services/test.service';
 	templateUrl: './notification.component.html',
 	styleUrls: ['./notification.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	providers: [TestService],
 })
-export class NotificationComponent implements OnInit, OnChanges {
-	@Input() notifications: INotification[] = [];
+export class NotificationComponent {
+	@Input() notifications: INotification[] | null = [];
 	@Output() snoozedNotification = new EventEmitter<INotification[]>();
 	@Output() test = new EventEmitter<INotification[]>();
-	public copyOfNotifications: INotification[];
-	public currentOfNotifications: INotification[];
 	public hour = 5000;
-	constructor(private _cdr: ChangeDetectorRef, public dataService: TestService) {
-		// this.copyOfNotifications = this.notifications;
-	}
-
-	public ngOnInit(): void {
-		// console.log(this.copyOfNotifications);
-		this.copyOfNotifications = this.notifications;
-		// this.dataService.getData().subscribe((config) => (this.copyOfNotifications = config));
-		// this.dataService.config$.subscribe((config) => (this.copyOfNotifications = config));
-	}
-
-	public ngOnChanges(): void {
-		// console.log(this.copyOfNotifications);
-		this.copyOfNotifications = this.currentOfNotifications;
-		this.dataService.getData().subscribe((config) => (this.copyOfNotifications = config));
-		this.dataService.config$.subscribe((config) => (this.copyOfNotifications = config));
-	}
+	constructor(private _cdr: ChangeDetectorRef, public dataService: TestService) {}
 
 	public snoozeNotification(i: number): void {
-		const notification = this.copyOfNotifications.splice(i, 1);
-		setTimeout(() => {
-			this.copyOfNotifications.unshift(notification[0]);
-			// this.dataService.changeConfig(this.copyOfNotifications);
-			this._cdr.detectChanges();
-		}, this.hour);
-		this.test.emit(this.copyOfNotifications);
+		this.test.emit(this.notifications?.filter((notification, index) => index !== i));
 	}
 
 	public dismissNotification(i: number): void {
-		this.copyOfNotifications.splice(i, 1);
+		this.test.emit(this.notifications?.filter((notification, index) => index !== i));
 	}
 
 	public dismissNotificationAll(): void {
-		this.copyOfNotifications.length = 0;
+		this.test.emit([]);
 	}
 
 	public snoozeNotificationAll(): void {
-		const notifications = this.copyOfNotifications;
-		this.copyOfNotifications = [];
-		setTimeout(() => {
-			this.copyOfNotifications = notifications;
-			this.notifications = this.copyOfNotifications;
-			this._cdr.detectChanges();
-		}, this.hour);
-		this.notifications = this.copyOfNotifications;
+		this.test.emit([]);
 	}
 }
