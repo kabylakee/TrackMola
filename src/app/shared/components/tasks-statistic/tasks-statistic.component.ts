6288e559e4bbc17e0308dc04333a2bf93f814b75
kirill-herman <kirill.herman.public@gmail.com>
2022-07-17 16:54:05 +0300

    feat: add weekly statistic chart

--- src/app/shared/components/tasks-statistic/tasks-statistic.component.ts
+++ src/app/shared/components/tasks-statistic/tasks-statistic.component.ts
@@ -1,5 +1,8 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import {Chart} from 'angular-highcharts';
+import {DEFAULT_TIME, DEFAULT_WEEK_WORKTIME} from 'src/app/entities/constants/hours.constants';
+import {IHours} from 'src/app/entities/interfaces/hours.interface';
+import {ITask} from 'src/app/entities/interfaces/task.interface';
 
 @Component({
 	selector: 'app-tasks-statistic',
@@ -7,63 +10,73 @@ import {Chart} from 'angular-highcharts';
 	styleUrls: ['./tasks-statistic.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TasksStatisticComponent {
-	public chart = new Chart({
-		chart: {
-			type: 'column',
-		},
-		title: {
-			text: '',
-		},
-		xAxis: {
-			type: 'category',
-			labels: {
-				rotation: -45,
-				x: 8,
-				style: {
-					fontSize: '10px',
-					lineHeight: '12px',
-					color: '#9DA1BC',
-				},
-			},
-		},
-		yAxis: {
-			min: 0,
-			title: {
-				text: 'Hours',
-				y: 40,
-				style: {
-					fontSize: '10px',
-					lineHeight: '12px',
-					color: '#9DA1BC',
-				},
-			},
-		},
-		tooltip: {
-			enabled: false,
-		},
-		credits: {
-			enabled: false,
-		},
-		series: [
-			{
+export class TasksStatisticComponent implements OnInit {
+	@Input() tasks: ITask[];
+
+	public chart: Chart;
+	private statisticTasks: [string, number][] = [];
+	public readonly weekNormalTime = DEFAULT_WEEK_WORKTIME;
+	public sumTime: IHours = DEFAULT_TIME;
+
+	public ngOnInit(): void {
+		this.sumTime = {
+			time: this.tasks.reduce((time, task) => (time += task.time), 0),
+			overtime: this.tasks.reduce((overtime, task) => (overtime += task.overtime), 0),
+		};
+		this.tasksToStatistic = this.tasks;
+		this.chart = new Chart({
+			chart: {
 				type: 'column',
-				colorByPoint: true,
-				data: [['AMCN PSVOD', 4]],
-				showInLegend: false,
-				// dataLabels: {
-				// 	enabled: true,
-				// 	rotation: -90,
-				// 	color: '#FFFFFF',
-				// 	align: 'right',
-				// 	format: '{point.y:.1f}', // one decimal
-				// 	y: 10, // 10 pixels down from the top
-				// 	style: {
-				// 		fontSize: '13px',
-				// 		fontFamily: 'Verdana, sans-serif',
-				// 	},
-				// },
+				height: 224,
 			},
-		],
-	});
+			title: {
+				text: '',
+			},
+			xAxis: {
+				type: 'category',
+				labels: {
+					rotation: -45,
+					x: 8,
+					style: {
+						fontSize: '10px',
+						lineHeight: '12px',
+						color: '#9DA1BC',
+					},
+				},
+			},
+			yAxis: {
+				min: 0,
+				title: {
+					text: 'Hours',
+					y: 20,
+					style: {
+						fontSize: '10px',
+						lineHeight: '12px',
+						color: '#9DA1BC',
+					},
+				},
+			},
+			tooltip: {
+				headerFormat: '{point.key}',
+				pointFormat: '',
+			},
+			credits: {
+				enabled: false,
+			},
+			series: [
+				{
+					type: 'column',
+					colorByPoint: true,
+					data: this.statisticTasks,
+					showInLegend: false,
+				},
+			],
+		});
+	}
+
+	private set tasksToStatistic(tasks: ITask[]) {
+		tasks.forEach((task) => {
+			this.statisticTasks.push([task.title, task.time]);
+		});
+	}
 }
--- src/app/shared/components/tasks-statistic/tasks-statistic.module.ts
+++ src/app/shared/components/tasks-statistic/tasks-statistic.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {TasksStatisticComponent} from './tasks-statistic.component';
 import {ChartModule} from 'angular-highcharts';
+import {HoursCardModule} from '../hours-card/hours-card.module';
 
 @NgModule({
 	declarations: [TasksStatisticComponent],
-	imports: [CommonModule, ChartModule],
+	imports: [CommonModule, ChartModule, HoursCardModule],
 	exports: [TasksStatisticComponent],
 })
 export class TasksStatisticModule {}
