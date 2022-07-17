43f02e3f1c3484ef99dd69e23c178adeec45b538
SeltikeS <pschenka@gmail.com>
2022-07-17 10:47:02 +0300

    add team list

--- src/app/entities/constants/project.mock.ts
+++ src/app/entities/constants/project.mock.ts
@@ -3,11 +3,14 @@ import {IProject} from '../interfaces/project.interface';
 export const PROJECT_MOCK: IProject[] = [
 	{
 		title: 'Project A',
+		employees: [],
 	},
 	{
 		title: 'Project B',
+		employees: [],
 	},
 	{
 		title: 'Project C',
+		employees: [],
 	},
 ];
--- src/app/shared/components/project-teams/project-teams.component.ts
+++ src/app/shared/components/project-teams/project-teams.component.ts
@@ -1,5 +1,7 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {EMPLOYEE_MOCK} from 'src/app/entities/constants/employee.mock';
 import {PROJECT_MOCK} from 'src/app/entities/constants/project.mock';
+import {IEmployee} from 'src/app/entities/interfaces/employee.interface';
 import {IProject} from 'src/app/entities/interfaces/project.interface';
 
 @Component({
@@ -9,6 +11,40 @@ import {IProject} from 'src/app/entities/interfaces/project.interface';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProjectTeamsComponent {
-	public readonly projects: IProject[] = PROJECT_MOCK;
+	public projects: IProject[] = PROJECT_MOCK;
 	public currentProject: IProject = PROJECT_MOCK[0];
+
+	public employees: IEmployee[] = EMPLOYEE_MOCK;
+
+	constructor() {
+		this.getEmployyes(PROJECT_MOCK);
+	}
+
+	private getEmployyes(projects: IProject[]): void {
+		projects.forEach((project: IProject) => {
+			this.employees.forEach((employee: IEmployee) => {
+				if (this.checkProject(employee.projects, project)) {
+					project.employees?.push(employee);
+				}
+			});
+		});
+	}
+
+	private checkProject(projectsArr: IProject[], project: IProject): boolean {
+		let result: boolean = false;
+		projectsArr.forEach((p) => {
+			if (p.title === project.title) {
+				result = true;
+			}
+		});
+		return result;
+	}
+
+	selectProject(value: Event) {
+		this.projects.forEach((project) => {
+			if (project.title === `${value}`) {
+				this.currentProject = project;
+			}
+		});
+	}
 }
