23b28b501b39947f74dbda18a3a477c6f2a6b087
Tatiana Kudryavets <minsk_tania@mail.ru>
2022-07-21 23:10:19 +0300

    Days left added

--- src/app/shared/components/vacation-request-form/vacation-request-form.component.ts
+++ src/app/shared/components/vacation-request-form/vacation-request-form.component.ts
@@ -20,6 +20,10 @@ export class VacationRequestFormComponent {
 	public requestForm: FormGroup;
 	public message: string;
 
+	public totalDays = 25;
+	public daysUsed = 14;
+	public daysNumber = true;
+
 	constructor(
 		private dialogRef: MatDialogRef<VacationRequestFormComponent>,
 		@Inject(MAT_DIALOG_DATA) public data: object,
@@ -48,25 +52,47 @@ export class VacationRequestFormComponent {
 		if (!control.get('dateFrom')?.value || !control.get('dateTo')?.value) {
 			return {error: true};
 		}
-		if (control.get('dateTo')?.value >= control.get('dateFrom')?.value) {
+		if (
+			!control.get('paid')?.value &&
+			control.get('dateTo')?.value >= control.get('dateFrom')?.value
+		) {
+			return null;
+		}
+
+		let selectedDays =
+			(control.get('dateTo')?.value.getTime() - control.get('dateFrom')?.value.getTime()) /
+			(1000 * 3600 * 24);
+		if (control.get('paid')?.value && (selectedDays === 11 || selectedDays === 14)) {
 			return null;
 		}
 		return {error: true};
 	}
 
 	public onSend(): void {
-		if (!this.requestForm.controls.dateFrom.valid) {
-			this.message = 'Date from is invalid';
+		if (!this.requestForm.controls.dateFrom.valid || !this.requestForm.controls.dateTo.valid) {
+			this.message = 'Date is invalid';
 			return;
 		}
-		if (!this.requestForm.controls.dateTo.valid) {
-			this.message = 'Date to is invalid';
+
+		let selectedDays =
+			(this.requestForm.controls.dateTo.value.getTime() -
+				this.requestForm.controls.dateFrom.value.getTime()) /
+			(1000 * 3600 * 24);
+
+		if (this.requestForm.controls.paid.value && selectedDays > this.totalDays - this.daysUsed) {
+			this.message = 'Choose less days';
+			this.daysNumber = false;
+			return;
+		}
+		if (this.requestForm.controls.paid.value && selectedDays !== 11 && selectedDays !== 14) {
+			this.message = 'Choose only 11 or 14 paid days';
 			return;
 		}
 		if (!this.requestForm.valid) {
-			this.message = 'Date to is invalid';
+			this.message = 'Date is invalid';
 			return;
 		}
+
 		this.sendRequest.emit(this.requestForm);
 		this.dialogRef.close();
 	}
