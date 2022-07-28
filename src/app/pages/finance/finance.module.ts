32700fcfafb80439b2f8f9ce8a47cced2df5b6fd
Tatiana Kudryavets <minsk_tania@mail.ru>
2022-07-29 00:59:52 +0300

    feature: header created

--- src/app/pages/finance/finance.module.ts
+++ src/app/pages/finance/finance.module.ts
@@ -3,9 +3,10 @@ import {CommonModule} from '@angular/common';
 import {FinanceComponent} from './finance.component';
 import {FinanceRoutingModule} from './finance-routing.module';
 import {HeaderModule} from 'src/app/shared/components/header/header.module';
+import {FinanceHeaderModule} from 'src/app/shared/components/finance-header/finance-header.module';
 
 @NgModule({
 	declarations: [FinanceComponent],
-	imports: [CommonModule, HeaderModule, FinanceRoutingModule],
+	imports: [CommonModule, HeaderModule, FinanceRoutingModule, FinanceHeaderModule],
 })
 export class FinanceModule {}
---
+++ src/app/shared/components/finance-header/finance-header.module.ts
@@ -0,0 +1,30 @@
+import {FinanceHeaderComponent} from './finance-header.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {DatePickerModule} from 'src/app/shared/components/date-picker/date-picker.module';
+import {ProjectModule} from '../project/projects.module';
+import {MatSelectModule} from '@angular/material/select';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {InputSearchModule} from '../input-search/input-search.module';
+
+@NgModule({
+	declarations: [FinanceHeaderComponent],
+	imports: [
+		CommonModule,
+		MatIconModule,
+		MatButtonModule,
+		DatePickerModule,
+		ProjectModule,
+		MatSelectModule,
+		MatDatepickerModule,
+		MatNativeDateModule,
+		MatFormFieldModule,
+		InputSearchModule,
+	],
+	exports: [FinanceHeaderComponent],
+})
+export class FinanceHeaderModule {}
