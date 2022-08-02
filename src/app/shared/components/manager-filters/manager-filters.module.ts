7cc7ba70dd0c658972cffe0b79e433c6eab894e4
kirill-herman <kirill.herman.public@gmail.com>
2022-08-02 09:47:23 +0300

    feat: add project select component

--- src/app/shared/components/manager-filters/manager-filters.module.ts
+++ src/app/shared/components/manager-filters/manager-filters.module.ts
@@ -5,10 +5,18 @@ import {DatePickerModule} from '../date-picker/date-picker.module';
 import {InputSearchModule} from '../input-search/input-search.module';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {ProjectSelectModule} from '../project-select/project-select.module';
 
 @NgModule({
 	declarations: [ManagerFiltersComponent],
-	imports: [CommonModule, DatePickerModule, InputSearchModule, MatButtonModule, MatIconModule],
+	imports: [
+		CommonModule,
+		DatePickerModule,
+		InputSearchModule,
+		MatButtonModule,
+		MatIconModule,
+		ProjectSelectModule,
+	],
 	exports: [ManagerFiltersComponent],
 })
 export class ManagerFiltersModule {}
---
+++ src/app/shared/components/project-select/project-select.component.ts
@@ -0,0 +1,31 @@
+import {ChangeDetectionStrategy, Component, EventEmitter, Output} from '@angular/core';
+import {PROJECT_MOCK} from 'src/app/entities/constants/project.mock';
+import {IProject} from 'src/app/entities/interfaces/project.interface';
+
+@Component({
+	selector: 'app-project-select',
+	templateUrl: './project-select.component.html',
+	styleUrls: ['./project-select.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProjectSelectComponent {
+	@Output() changeProject = new EventEmitter<string>();
+
+	public projects: IProject[] = PROJECT_MOCK;
+	public currentProject: IProject = PROJECT_MOCK[0];
+
+	public selectProject(value: Event): void {
+		this.projects.forEach((project) => {
+			if (project.title === `${value}`) {
+				this.currentProject = project;
+			}
+		});
+		this.changeProject.emit(this.currentProject.title);
+	}
+
+	public getColor(): {[k: string]: string} {
+		return {
+			color: `rgb(${this.currentProject.color})`,
+		};
+	}
+}
