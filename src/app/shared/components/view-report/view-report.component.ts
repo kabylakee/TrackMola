15da67c9bb7aad8edba094042795778bcba1c57a
SeltikeS <pschenka@gmail.com>
2022-08-05 09:13:12 +0300

    Add table config

--- src/app/shared/components/view-report/view-report.component.ts
+++ src/app/shared/components/view-report/view-report.component.ts
@@ -1,5 +1,8 @@
 import {ChangeDetectionStrategy, Component, Inject} from '@angular/core';
 import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {MODAL_TABLE_CONFIG} from 'src/app/entities/constants/day-columns.config';
+import {ITableColumn} from 'src/app/entities/interfaces/table-column.interface';
+import {ITask} from 'src/app/entities/interfaces/task.interface';
 
 @Component({
 	selector: 'app-view-report',
@@ -8,8 +11,13 @@ import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ViewReportComponent {
+	public tasks: ITask[];
+	public columns: ITableColumn[];
+
 	constructor(
 		private dialogRef: MatDialogRef<ViewReportComponent>,
 		@Inject(MAT_DIALOG_DATA) public data: object,
-	) {}
+	) {
+		this.columns = MODAL_TABLE_CONFIG;
+	}
 }
