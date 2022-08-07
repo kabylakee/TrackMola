f29a020bd01144ac3c8e20eeaff24a4d808f7324
kirill-herman <kirill.herman.public@gmail.com>
2022-08-07 15:22:00 +0300

    fix: make surname required field

--- src/app/entities/interfaces/excel-data.interface.ts
+++ src/app/entities/interfaces/excel-data.interface.ts
@@ -2,7 +2,7 @@ export interface IExcelData {
 	project?: string;
 	deliverables?: string;
 	resourse?: string;
-	surname?: string;
+	surname: string;
 	percent?: string;
 	workingHours?: string;
 	hours?: string;
