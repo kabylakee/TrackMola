001cec784600a8390e36584539c4a9451b002d7c
Tatiana Kudryavets <minsk_tania@mail.ru>
2022-08-10 12:40:07 +0300

    select all fixed

--- src/app/shared/components/project-select/project-select.component.ts
+++ src/app/shared/components/project-select/project-select.component.ts
@@ -1,4 +1,11 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Output} from '@angular/core';
+import {
+	ChangeDetectionStrategy,
+	Component,
+	EventEmitter,
+	Input,
+	OnInit,
+	Output,
+} from '@angular/core';
 import {PROJECT_MOCK} from 'src/app/entities/constants/project.mock';
 import {IProject} from 'src/app/entities/interfaces/project.interface';
 import {SELECT_ALL} from '../../../entities/constants/formats.constants';
@@ -9,13 +16,20 @@ import {SELECT_ALL} from '../../../entities/constants/formats.constants';
 	styleUrls: ['./project-select.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProjectSelectComponent {
+export class ProjectSelectComponent implements OnInit {
+	@Input() isSelectAll = true;
+
 	@Output() changeProject = new EventEmitter<string>();
 
 	public projects: IProject[] = PROJECT_MOCK;
 	public currentProject = SELECT_ALL;
 	public readonly SELECT_ALL = 'Select All';
 
+	public ngOnInit(): void {
+		if (this.isSelectAll) {
+			this.currentProject = SELECT_ALL;
+		} else this.currentProject = this.projects[0].title;
+	}
 	public selectProject(value: Event): void {
 		this.currentProject =
 			value + '' === SELECT_ALL
